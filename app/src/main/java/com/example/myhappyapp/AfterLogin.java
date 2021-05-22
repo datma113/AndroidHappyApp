31b446c3429d544a63f9e1d25b80d25764f3b009
datma <datma113112111@gmail.com>
2021-05-22 18:40:23 +0700

    first comm

---
+++ app/src/main/java/com/example/myhappyapp/AfterLogin.java
@@ -0,0 +1,15 @@
+package com.example.myhappyapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AfterLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.after_login_layout);
+
+    }
+}
